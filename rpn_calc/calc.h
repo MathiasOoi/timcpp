12e34aa294dbfec1a6c6c89b9277c93f788432a4
MathiasOoi <60244997+MathiasOoi@users.noreply.github.com>
2021-01-09 19:19:33 -0800

    RPN Calculator Part 2

--- rpn_calc/calc.h
+++ rpn_calc/calc.h
@@ -1,6 +1,9 @@
 #include <vector>
 #include <string>
 #include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <sstream>
 
 using namespace std;
 
@@ -13,23 +16,30 @@ class Stack{
         void push(const double &item) {v.push_back(item);}
         double &top() {return v[v.size() - 1];}
         void pop() {v.pop_back();}
-        void print(){
-            cout << "[";
+        string repr(){
+            ostringstream oss;
+            oss << "[";
             if (!v.empty()){
                 for (int i = 0; i < v.size() - 1; ++i){
-                    cout << v[i] << ", ";
+                    oss << v[i] << ", ";
                 }
-                cout << v[v.size() - 1] << "]" << endl;
+                oss << v[v.size() - 1] << "]" << endl;
             }
+            return oss.str();
         }
 };
 
 class Calc{
     private:
         Stack stack;
-        unordered_set<string> operators = {"+", "-", "/", "*"};
+        unordered_map<string, function<double(Stack*)> > functions;
+        unordered_map<string, double> variables;
+        bool store_var = false, set_var = false;
     public:
         void insert(const string token);
-        void print() {stack.print();};
+        string repr() {return stack.repr();}
         string get() {return to_string(stack.top());}
+        void register_fn(string name, function<double(Stack*)> new_fn);
+        void store (string var_name);
+        void set(string var_name);
 };
